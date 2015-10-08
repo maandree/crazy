98b21f57539b0ebc6aa3a73b7ee37b204785aa1e
Mattias Andrée <maandree@operamail.com>
2015-10-09 00:55:39 +0200

    add common.h
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

---
+++ src/tools/common.h
@@ -0,0 +1,32 @@
+/**
+ * crazy — A crazy simple and usable scanning utility
+ * Copyright © 2015  Mattias Andrée (maandree@member.fsf.org)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef CRAZY_TOOLS_COMMON_H
+#define CRAZY_TOOLS_COMMON_H
+
+
+
+int copyfile(char* src, char* dest);
+int symlfile(char* src, char* dest);
+int linkfile(char* src, char* dest);
+int movefile(char* src, char* dest);
+int mkdirs(char* dir);
+
+
+
+#endif
+
