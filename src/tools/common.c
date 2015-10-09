acdb62a830723890e3255e33577a66e0f59b328f
Mattias Andrée <maandree@operamail.com>
2015-10-09 14:11:02 +0200

    implement symlfile
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- src/tools/common.c
+++ src/tools/common.c
@@ -15,6 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#define _GNU_SOURCE
 #include "common.h"
 #include <alloca.h>
 #include <errno.h>
@@ -22,9 +23,130 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <limits.h>
 #include <sys/stat.h>
 #include <sys/sendfile.h>
 
+
+
+/**
+ * Get the absolute path of a file,
+ * it will remove all redundant slashes, all ./:s, and
+ * all ../:s, but not resolve symbolic links
+ * 
+ * @param   file  The file
+ * @param   ref   The current working directory
+ * @return        The file's absolute pathname
+ */
+static char* abspath(const char* file, const char* ref)
+{
+  char* rc;
+  char* rc_;
+  size_t p, q;
+  
+  rc = malloc((strlen(file) + strlen(ref) + 2) * sizeof(char));
+  if (rc_ = rc, rc == NULL)
+    return NULL;
+  
+  /* Create absolute path, with //, /./, and /../. */
+  if (*file != '/')
+    {
+      rc_ = strcpy(rc_, ref);
+      *rc_++ = '/';
+    }
+  strcpy(rc_, file);
+  
+  /* Remove redundant slashes. */
+  p = q = 1;
+  while (rc[p])
+    if ((rc[p] == '/') && (rc[p - 1] == '/'))
+      p++;
+    else
+      rc[q++] = rc[p++];
+  rc[q] = '\0';
+  
+  /* Remove ./:s. */
+  p = q = 0;
+  while (rc[p])
+    if ((rc[p] == '/') && (rc[p + 1] == '.') && (rc[p + 2] == '/'))
+      p += 2;
+    else
+      rc[q++] = rc[p++];
+  rc[q] = '\0';
+  
+  /* Remove ../:s. */
+  for (;;)
+    {
+      rc_ = strstr(rc, "/../");
+      if (rc_ == NULL)
+	break;
+      memmove(rc_, rc_ + 3, strlen(rc_ + 2) * sizeof(char));
+      if (rc_ == rc)
+	continue;
+      for (p = 0; rc_[-p] != '/'; p++);
+      memmove(rc_ - p, rc_, (strlen(rc_) + 1) * sizeof(char));
+    }
+  
+  return rc;
+}
+
+
+/**
+ * Get the relative path of a file
+ * 
+ * @param   file  The file
+ * @param   ref   The reference file (not directory)
+ * @return        The file's pathname relative to the reference file
+ */
+static char* relpath(const char* file, const char* ref)
+{
+  int saved_errno;
+  char* cwd = NULL;
+  char* absfile = NULL;
+  char* absref = NULL;
+  char* rc = NULL;
+  char* prc;
+  size_t ptr, p, n = 0;
+  
+  if ((cwd     = get_current_dir_name()) == NULL)  goto fail;
+  if ((absfile = abspath(file, cwd))     == NULL)  goto fail;
+  if ((absref  = abspath(ref, cwd))      == NULL)  goto fail;
+  
+  strrchr(absref, '/')[1] = '\0';
+  
+  ptr = 0;
+  while (absfile[ptr] && (absfile[ptr] == absref[ptr]))
+    ptr++;
+  while (absfile[ptr] != '/')
+    ptr--;
+  
+  for (p = ptr; absref[p]; p++)
+    if (absref[p] == '/')
+      n += 1;
+  
+  rc = malloc((strlen(absfile + ptr) + 1 + 3 * n) * sizeof(char));
+  if (prc = rc, rc == NULL)
+    goto fail;
+  
+  while (n--)
+    prc = stpcpy(prc, "../");
+  strcpy(prc, absfile + ptr);
+  
+  free(cwd);
+  free(absfile);
+  free(absref);
+  return rc;
+  
+ fail:
+  saved_errno = errno;
+  free(cwd);
+  free(absfile);
+  free(absref);
+  free(rc);
+  errno = saved_errno;
+  return NULL;
+}
 
 
 /**
@@ -79,6 +201,23 @@ int copyfile(const char* src, const char* dest)
  */
 int symlfile(const char* src, const char* dest)
 {
+  char* target = NULL;
+  int saved_errno;
+  
+  target = relpath(src, dest);
+  if (target == NULL)
+    goto fail;
+  
+  if (symlink(target, dest))
+    goto fail;
+  
+  free(target);
+  return 0;
+ fail:
+  saved_errno = errno;
+  free(target);
+  errno = saved_errno;
+  return -1;
 }
 
 
